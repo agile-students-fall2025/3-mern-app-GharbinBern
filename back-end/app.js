494a88cf767cd6c477fe50af6d616e3d44b7da08
GharbinBern <bg2696@nyu.edu>
2025-10-03 22:34:06 -0500

    Add /aboutus API route to fetch about us data from database

--- back-end/app.js
+++ back-end/app.js
@@ -21,6 +21,7 @@ mongoose
 // load the dataabase models we want to deal with
 const { Message } = require('./models/Message')
 const { User } = require('./models/User')
+const { AboutUs } = require('./models/Aboutus')
 
 // a route to handle fetching all messages
 app.get('/messages', async (req, res) => {
@@ -74,6 +75,23 @@ app.post('/messages/save', async (req, res) => {
     return res.status(400).json({
       error: err,
       status: 'failed to save the message to the database',
+    })
+  }
+})
+//a route to handle fetching about us info
+app.get('/aboutus', async (req, res) => {
+  // load info from database
+  try {
+    const about = await AboutUs.find({})
+    res.json({
+      about: about,
+      status: 'all good',
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: 'failed to retrieve about us info from the database',
     })
   }
 })
